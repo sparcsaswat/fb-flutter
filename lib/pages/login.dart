7e8685cd9731b0b6ff3515c66258226ace8d1219
saswatsaubhagya <saswatsaubhagya@gmail.com>
2019-07-11 22:26:34 +0530

    login page design modified

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -19,14 +19,110 @@ class _LoginPageState extends State<LoginPage> {
           children: <Widget>[
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                
+              height: MediaQuery.of(context).size.height / 2.6,
+              child: ClipPath(
+                clipper: ClippingClass(),
+                child: Container(
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Center(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: Image.asset("assets/logobuddy.png"),
+                    ),
+                  ),
+                ),
               ),
             ),
+            Container(
+              padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      cursorColor: Colors.white,
+                      style: TextStyle(color: Colors.white),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          prefixIcon: Icon(
+                            Icons.person_outline,
+                            color: Colors.white,
+                          ),
+                          hintText: "Email id",
+                          hintStyle: TextStyle(color: Colors.white)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0),
+                    ),
+                    TextFormField(
+                      cursorColor: Colors.white,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          prefixIcon: Icon(
+                            Icons.lock_outline,
+                            color: Colors.white,
+                          ),
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Colors.white)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40.0),
+                    ),
+                    RaisedButton(
+                      padding: EdgeInsets.only(left: 40.0,right: 40.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      color: Color.fromRGBO(127, 168, 235, 1),
+                      onPressed: () {},
+                      child: Text('LOGIN'),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 25.0)),
+                    Text(
+                      'New Member?',
+                      style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+                    ),
+                    RaisedButton(
+                      padding: EdgeInsets.only(left: 40.0,right: 40.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      color: Color.fromRGBO(127, 168, 235, 1),
+                      onPressed: () {},
+                      child: Text('SIGN UP'),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 40);
+    path.quadraticBezierTo(
+        size.width / 4, size.height, size.width / 2, size.height);
+    path.quadraticBezierTo(size.width - (size.width / 4), size.height,
+        size.width, size.height - 40);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
