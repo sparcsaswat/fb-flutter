469e2da097f5a1f5389278357944bd2c15516bb6
Saswat Saubhagya Rout <saswat.srout@sparcindia.com>
2019-07-11 19:37:56 +0530

    login screen added

---
+++ lib/pages/login.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(45, 72, 156, 1),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/initpage.dart
+++ lib/utils/initpage.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-import '../pages/homepage.dart';
+// import '../pages/homepage.dart';
 
-void initPage(BuildContext context) async {
+// void initPage(BuildContext context) async {
   
-  var prefs = await SharedPreferences.getInstance();
-  final result = prefs.getString('uid');
-  if (result != null) {
-    if (result.isNotEmpty) {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (BuildContext context) => HomePage()));
-    }
-  }
-}
+//   var prefs = await SharedPreferences.getInstance();
+//   final result = prefs.getString('uid');
+//   if (result != null) {
+//     if (result.isNotEmpty) {
+//       Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(
+//               builder: (BuildContext context) => HomePage()));
+//     }
+//   }
+// }
